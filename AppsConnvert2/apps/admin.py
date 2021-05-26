51f42eed158cee02c9f65ff6e0b026942ec545c0
Zack Mariano <zack.cmariano@gmail.com>
2021-05-26 11:03:12 -0300

    -

--- AppsConnvert2/apps/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from apps.models import Usuario
-
-class Usuarios(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'email')
-    list_display_links = ('id', 'nome')
-    search_fields = ('nome',)
-
-admin.site.register(Usuario, Usuarios)
--- AppsConnvert2/apps/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from apps.models import Usuario
-
-
-class UsuarioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Usuario
-        fields = ['id', 'nome', 'email']
